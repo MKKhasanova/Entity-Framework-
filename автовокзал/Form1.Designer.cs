7f44e9a4e520975e88a9f1bbf211664703ec53cd
Mariyam Khasanova <mariyam9916@icloud.com>
2024-05-28 12:53:53 +0300

    Добавьте файлы проекта.

---
+++ автовокзал/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace автовокзал
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(745, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+           
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(778, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти билеты";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(280, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Автовокзал Набережные Челны";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Куда";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Проходящие автобусы";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(777, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Маршрут";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Введите дату:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 329);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Выбор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Отказ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 357);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(745, 150);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(16, 537);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(751, 150);
+            this.dataGridView3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 691);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ автовокзал/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace автовокзал
+{
+    public partial class Form1 : Form
+    {
+        avtovokEntities db = new avtovokEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // UpdateDataGridViewBefore();
+            //UpdateDataGridView1();
+        }
+        private void UpdateDataGridViewBefore()//обновление dataGridView1
+        {
+            var routes_ = from routes in db.Rout
+                          join town in db.Town on routes.Town_ID equals town.Town_ID
+                          select new
+                          {
+                              Routes_ID = routes.Rout_ID,
+                              Routes_Number = routes.Routes_number,
+                              Departure = routes.Otk,
+                              Departure_Time = routes.Vrem_otp,
+                              Destination = town.Town1,
+                              Destination_Time = routes.Vrem_Prib,
+                              Koll_Biletov = routes.Kol,
+                              Info = routes.Info
+                          };
+            dataGridView3.DataSource = routes_.ToList();
+        }
+        private void UpdateDataGridView1()//обновление dataGridView2
+        {
+            var stopover = from stopoveR in db.Stopover
+                           join village in db.Village on stopoveR.Village_ID equals village.Village_ID
+                           join rout in db.Rout on stopoveR.Routes_ID equals rout.Rout_ID
+                           select new
+                           {
+                               Routes_Namber = rout.Routes_number,
+                               Data = textBox2.Text,
+                               Departure = "Набережные Челны",
+                               Vremya_otp_iz_NCH = rout.Vrem_Prib_NCH,
+                               Village = village.Village_Name,
+                               Vremiya_Prib = rout.Vrem_Prib,
+                               Price = stopoveR.Price
+                           };
+            dataGridView2.DataSource = stopover.ToList();
+        }
+
+       /* private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0) // Убедимся, что была кликнута не заголовочная ячейка
+            {
+                int routesID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Routes_Number"].Value); // Получаем номер маршрута из выбранной строки
+
+                var selectedRoutes = db.Rout.FirstOrDefault(m => m.Routes_number == routesID); // Находим соответствующую запись в базе данных
+
+                if (selectedRoutes != null)
+                {
+                    string inf = selectedRoutes.Info; // Получаем информацию о маршруте
+                    string[] infoParts = inf.Split(' '); // Разбиваем информацию на части
+
+                    string labelText = ""; // Подготовим текст для label
+                    for (int i = 0; i < infoParts.Length - 1; i += 2) // Теперь используем шаг 2, так как у нас две части информации (время и город)
+                    {
+                        string time = infoParts[i];
+                        string city = infoParts[i + 1];
+
+                        labelText += time + " " + city + Environment.NewLine; // Добавляем время и город в labelText с переводом строки
+                    }
+
+                    label1.Text = labelText; // Устанавливаем labelText в label
+                }
+            }
+        }*/
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filterText = textBox1.Text.ToLower();
+
+            var filteredData = (from stopoveR in db.Stopover
+                                join village in db.Village on stopoveR.Village_ID equals village.Village_ID
+                                join rout in db.Rout on stopoveR.Routes_ID equals rout.Rout_ID
+                                join town in db.Town on rout.Town_ID equals town.Town_ID
+                                where village.Village_Name.ToLower().Contains(filterText) || town.Town1.ToLower().Contains(filterText)
+                                select new
+                                {
+                                    Routes_ID = rout.Rout_ID,
+                                    Routes_Number = rout.Routes_number,
+                                    Departure = rout.Otk,
+                                    // Departure_Time = rout.Vrem_otp,
+                                    Destination = town.Town1,
+                                    //Destination_Time = rout.Vrem_Prib,
+                                    Koll_Biletov = rout.Kol,
+                                    Price = stopoveR.Price
+                                }).ToList();
+
+            dataGridView1.DataSource = filteredData;
+
+        }
+       
+   
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*DataGridViewRow curRow = dataGridView1.CurrentRow;
+            int Routes_Id = Convert.ToInt32(curRow.Cells["Routes_ID"].Value);
+            int Routes_Number = (int)curRow.Cells["Routes_Number"].Value;
+            decimal Pric = Convert.ToDecimal(curRow.Cells["Price"].Value);
+            string Koli = textBox2.Text;
+
+            Kor BiletToKorzina = new Kor();
+            BiletToKorzina.Id = Routes_Id;
+            BiletToKorzina.Routes_Number = Routes_Number;
+            BiletToKorzina.Data =Koli;
+            BiletToKorzina.C_Departure = 3;
+            BiletToKorzina.C_Departure_Time = "hd";
+            BiletToKorzina.C_Destination = 2;
+            BiletToKorzina.C_Destination_Time = "fhc";
+            BiletToKorzina.Price = Pric;
+
+
+
+            db.Kor.Add(BiletToKorzina);
+
+            var korzinas = from korzina in db.Kor
+                           join rout in db.Rout on korzina.Routes_Number equals rout.Rout_ID
+                           
+                           select new
+                           {
+                               Id=rout.Rout_ID,
+                               Routes_Number = rout.Routes_number,
+                              
+                               Data = Koli,
+                              
+                           };
+
+            dataGridView2.DataSource = korzinas.ToList();
+*/          DataGridViewRow curRow = dataGridView1.CurrentRow;
+            int Routes_ID = (int)curRow.Cells["Routes_ID"].Value;
+            int Routes_Number = (int)curRow.Cells["Routes_Number"].Value;
+            string data = textBox2.Text;
+            
+            MessageBox.Show(Routes_ID.ToString());
+            MessageBox.Show(data.ToString());
+
+            /*if (dataGridView3.CurrentCell != null) // Проверяем, что есть выбранная ячейка
+            {
+                int rowIndex = dataGridView3.CurrentCell.RowIndex;
+                int columnIndex = dataGridView3.CurrentCell.ColumnIndex;
+                
+                int Routes_ID = (int)curRow.Cells["Menu_ID"].Value;
+
+                // Выполняем действия в зависимости от индекса столбца
+                if (columnIndex == 0) // Пример для первого столбца
+                {
+                    MessageBox.Show($"Выбрана строка с id: {rowIndex}");
+                }
+                else if (columnIndex == 1) // Пример для второго столбца
+                {
+                    // Код для второго столбца
+                }
+                // И так далее для других столбцов
+            }
+            else
+            {
+                MessageBox.Show("Ячейка не выбрана");
+            }*/
+        }
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0) // Убедимся, что была кликнута не заголовочная ячейка
+            {
+                int routesID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Routes_Number"].Value); // Получаем номер маршрута из выбранной строки
+
+                var selectedRoutes = db.Rout.FirstOrDefault(m => m.Routes_number == routesID); // Находим соответствующую запись в базе данных
+
+                if (selectedRoutes != null)
+                {
+                    string inf = selectedRoutes.Info; // Получаем информацию о маршруте
+                    string[] infoParts = inf.Split(' '); // Разбиваем информацию на части
+
+                    string labelText = ""; // Подготовим текст для label
+                    for (int i = 0; i < infoParts.Length - 1; i += 2) // Теперь используем шаг 2, так как у нас две части информации (время и город)
+                    {
+                        string time = infoParts[i];
+                        string city = infoParts[i + 1];
+
+                        labelText += time + " " + city + Environment.NewLine; // Добавляем время и город в labelText с переводом строки
+                    }
+
+                    label1.Text = labelText; // Устанавливаем labelText в label
+                }
+            }
+        }
+
+      
+    }
+}
+
